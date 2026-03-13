c8511f301e91817138476f69d440c07ea1009101
chr0meH <dimaboiko001@gmail.com>
2026-03-13 09:45:45 +0200

    made a working calculator

---
+++ Calculator/Queue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Calculator
+{
+    public class Queue
+    {
+        private string[] _array = new string[50];
+        private int _first = 0;
+        private int _last = 0;
+        private int _count = 0;
+        public void Enqueue(string element) 
+        {
+            if (_count == 50) throw new Exception("Черга переповнена");
+            _array[_last] = element;
+            _last = (_last + 1) % 50;
+            _count++;
+        }
+
+        public string[] ToArray() 
+        {
+            string[] arr = new string[_count];
+            for (int i = 0; i < _count; i++) arr[i] = _array[(i+_first)%50];
+            return arr;
+        }
+        public string Dequeue()
+        { 
+            if(_count == 0) throw new Exception("Черга порожня");
+            string element = _array[_first];
+            _first = (_first + 1) % 50;
+            _count--;
+            return element;
+        }
+    }
+}
---
+++ Calculator/Stack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class Stack
+    {
+        private string[] _array = new string[50];
+        private int _pointer = 0;
+
+        public string Peek() { return _pointer == 0 ? null : _array[_pointer-1]; }
+        public void Push(string value) 
+        {
+            if (_pointer == _array.Length)
+            {
+                throw new Exception("Stack overflowed");
+            }
+            _array[_pointer] = value;
+            _pointer++;
+        }
+
+        public string Pop()
+        {
+            if (_pointer == 0)
+            {
+                throw new Exception("Stack is empty");
+            }
+            _pointer--;
+            var value = _array[_pointer];
+            return value;
+        }
+    }
+}
